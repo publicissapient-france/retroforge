0a211f3debd8ab89e4bae674305e09d073d2988b
Richard ROBEZ <richard.robez@publicissapient.com>
2025-03-24 16:27:50 +0100

    changing css

--- app/common/components/header/Header.tsx
+++ app/common/components/header/Header.tsx
@@ -1,5 +1,13 @@
+import { useTranslation } from 'react-i18next'
+
 import styles from './Header.module.css'
 
 export default function Header() {
-  return <header className={styles.header}>Header to be designed</header>
+  const { t } = useTranslation()
+
+  return (
+    <header className={`${styles.header} flex justify-center`}>
+      <h1 className="self-center">{t('title')}</h1>
+    </header>
+  )
 }
--- app/hammer-swipe/SwipeAction/SwipeAction.tsx
+++ app/hammer-swipe/SwipeAction/SwipeAction.tsx
@@ -14,7 +14,7 @@ export default function SwipeAction({ type, onClick }: SwipeActionProps) {
   const { t } = useTranslation()
 
   return (
-    <button className="flex justify-center items-center rounded-full cursor-pointer w-[80px] h-[80px] border border-[#d8d8d8]" onClick={() => onClick(type)}>
+    <button className="flex justify-center items-center rounded-full cursor-pointer w-[80px] h-[80px] border border-[#d8d8d8] hover:bg-[#fafafa]" onClick={() => onClick(type)}>
       {type === Action.YES && <img className="w-[35px] h-[35px]" src={YesImage} alt={t('actions.yes')} />}
       {type === Action.NO && <img className="w-[25px] h-[25px]" src={NoImage} alt={t('actions.no')} />}
     </button>
