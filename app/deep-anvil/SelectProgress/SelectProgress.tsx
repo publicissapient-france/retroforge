35a54bbe9e100af88caec03ee4375057860a57b0
Richard ROBEZ <richard.robez@publicissapient.com>
2025-04-14 12:12:37 +0200

    improve responsive design a bit

--- app/deep-anvil/SelectProgress/SelectProgress.tsx
+++ app/deep-anvil/SelectProgress/SelectProgress.tsx
@@ -1,5 +1,5 @@
 import { animate, motion, useMotionValue } from 'framer-motion'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export type SelectProgressProps = {
   progress: number
@@ -9,14 +9,21 @@ export type SelectProgressProps = {
 const CARD_WIDTH_LG = 800
 
 export function SelectProgress({ progress, total }: SelectProgressProps) {
+  const progressRef = useRef<HTMLDivElement>(null)
   const width = useMotionValue(Math.floor(CARD_WIDTH_LG / total) * progress)
 
   useEffect(() => {
-    animate(width, Math.floor(CARD_WIDTH_LG / total) * progress)
+    const elementWidth = progressRef?.current?.clientWidth ?? CARD_WIDTH_LG
+    animate(width, Math.ceil(elementWidth / total) * progress)
   }, [progress, total])
 
   return (
-    <div className={`w-[${CARD_WIDTH_LG}px] mb-5`}>
+    <motion.div
+      initial={{ x: -1500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: 'spring', stiffness: 75, damping: 15 }}      ref={progressRef}
+      className="lg:w-[800px] sm:w-[550px] max-sm:w-[300px] mb-5"
+    >
       <div className="relative">
         <div className="absolute w-full h-5 rounded-lg bg-blue-400" />
         <motion.div
@@ -25,6 +32,6 @@ export function SelectProgress({ progress, total }: SelectProgressProps) {
         />
         <div className="absolute flex h-5 justify-center items-center text-xs font-bold text-center w-full text-white">{progress} / {total}</div>
       </div>
-    </div>
+    </motion.div>
   )
 }
