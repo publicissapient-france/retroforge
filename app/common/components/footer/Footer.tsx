e7dbacacc4c6b0116e01e0f9ded1d15487980250
akuoch <47180478+akuoch@users.noreply.github.com>
2026-01-29 13:58:18 +0100

    Update Footer.tsx testing feedback link

--- app/common/components/footer/Footer.tsx
+++ app/common/components/footer/Footer.tsx
@@ -13,7 +13,8 @@ export default function Footer() {
         <img src={psLogo} width="55" alt="logo publicis sapient" />
       </div>
       <div className={styles.arrowContainer}>
-        <div className={`${styles.arrowText} dark:text-white text-[#555555]`}>{t('footer.whoAreWe')} <span className="text-(--accent-color)">?</span></div>
+        <div className={`${styles.arrowText} dark:text-white text-[#555555]`}>{t('footer.whoAreWe')} <span className="text-(--accent-color)">?</span>
+        <h1 className="align-self uppercase font-bold tracking-(--title-letter-spacing) text-(--accent-color) dark:text-(--accent-color-dark)"><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=oZ4s1SF8sUeCozOnSx90uGCkTilWip1PpA6dC9L_8JhURU9TU1pORlVRNEQ0VDM0S0pVRlBFNjlaTS4u" title="Linktitle" target="_blank" rel="noopener noreferrer">Share your feedback</a></h1></div>
         <FooterArrow className="dark:text-white" />
       </div>
     </div>
