2640f72d9d23cd24d4f2fc5fe3db83ad8a2e9484
akuoch <47180478+akuoch@users.noreply.github.com>
2026-01-29 14:44:10 +0100

    Update RetrospectiveMatched.tsx testing feedback link

--- app/common/components/RetrospectiveMatched/RetrospectiveMatched.tsx
+++ app/common/components/RetrospectiveMatched/RetrospectiveMatched.tsx
@@ -24,5 +24,8 @@ export function RetrospectiveMatched({ matched, className }: RetrospectiveMatche
       )}
       <RetrospectiveListing retrospectives={matched.additional} />
     </div>
+    <div>
+    <h3 className="right uppercase text-(--accent-color) dark:text-(--accent-color-dark)"><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=oZ4s1SF8sUeCozOnSx90uGCkTilWip1PpA6dC9L_8JhURU9TU1pORlVRNEQ0VDM0S0pVRlBFNjlaTS4u" title="Linktitle" target="_blank" rel="noopener noreferrer">Share your feedback</a></h3>
+    </div>
   )
 }
