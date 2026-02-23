6cc640397be37b61961a09e7d6d220c3e8446672
akuoch <47180478+akuoch@users.noreply.github.com>
2026-02-23 15:00:40 +0100

    Change default language from 'fr' to 'en'

--- app/common/hooks/UseCurrentLanguage.ts
+++ app/common/hooks/UseCurrentLanguage.ts
@@ -3,5 +3,5 @@ import { useTranslation } from 'react-i18next'
 export const useCurrentLanguage = () => {
   const { i18n } = useTranslation()
 
-  return { currentLanguage: i18n.language ?? 'fr' }
+  return { currentLanguage: i18n.language ?? 'en' }
 }
