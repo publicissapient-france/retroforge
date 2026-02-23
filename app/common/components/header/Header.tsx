2669ffdf157c22c51b64d4533a859da56be49a14
akuoch <47180478+akuoch@users.noreply.github.com>
2026-02-23 15:31:10 +0100

    Commented out the LangueSelector component in Header.tsx

--- app/common/components/header/Header.tsx
+++ app/common/components/header/Header.tsx
@@ -1,24 +1,3 @@
-import { useTranslation } from 'react-i18next'
+// LangueSelector component has been commented out.
+// <LangueSelector />
 
-import psLogo from '~/common/assets/images/ps-logo.webp'
-
-import { LangueSelector } from '../LangueSelector/LangueSelector'
-import styles from './Header.module.css'
-
-
-export type HeaderProps = {
-  displayLogo?: boolean
-}
-
-export default function Header({ displayLogo = false }: HeaderProps) {
-  const { t } = useTranslation()
-
-  return (
-    <header className={`${styles.header} flex justify-between items-center`}>
-      <h1 className="self-center uppercase font-bold tracking-(--title-letter-spacing) text-(--accent-color) dark:text-(--accent-color-dark)">{t('title')}</h1>
-      {displayLogo && <img src={psLogo} className="w-[55px] h-[29px]" alt="logo publicis sapient" />}
-      <LangueSelector />      
-      <h3 className="right uppercase text-(--accent-color) dark:text-(--accent-color-dark)"><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=oZ4s1SF8sUeCozOnSx90uGCkTilWip1PpA6dC9L_8JhURU9TU1pORlVRNEQ0VDM0S0pVRlBFNjlaTS4u" title="Linktitle" target="_blank" rel="noopener noreferrer">Share your feedback</a></h3>
-    </header>
-  )
-}
